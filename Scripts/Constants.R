cc22002176e21496f08514c145c5d17e8d1a6255
Lee Richman <leepr@upenn.edu>
2022-02-01 11:05:12 -0500

    update

--- Scripts/Constants.R
+++ Scripts/Constants.R
@@ -4,21 +4,23 @@ library(BarCluster)
 library(ggplot2)
 library(WebGestaltR)
 
+# NJ Neuro-directed iPSC alevel = 0.8
+
 # vector of short names
 sn_v <- c(paste("YG", 1:3, sep = ""), "Kleind2ws", "CJ",
-paste("MDA", 1:2, sep = ""), paste("WM983", 1:2, sep = ""), "NJ")
+paste("MDA", 1:2, sep = ""), paste("WM983", 1:2, sep = ""))
 
 # vector of long names
 tv <- c("High dose BRAF inhibitor\nReplicate 1", "High dose BRAF inhibitor\nReplicate 2",
 "Low dose BRAF inhibitor", "Weinreb et al. (2020)\nIn vitro hematopoiesis",
 "Cardio-directed iPSC", "Paclitaxel-treated breast cancer cells\nReplicate 1", "Paclitaxel-treated breast cancer cells\nReplicate 2",
-"WM983 BRAF inhibitor\nReplicate 1", "WM983 BRAF inhibitor\nReplicate 2", "Neuro-directed iPSC")
+"WM983 BRAF inhibitor\nReplicate 1", "WM983 BRAF inhibitor\nReplicate 2")
 
 names(tv) <- sn_v
 
 # cutoffs determined by max alpha before cluster number increases
 mt <- data.table::data.table(sample_id = sn_v,
-  alevel = c(0.55, 0.6, 0.6, 0.75, 0.55, 0.4, 0.7, 0.7, 0.65, 0.8))
+  alevel = c(0.55, 0.6, 0.6, 0.75, 0.55, 0.4, 0.7, 0.7, 0.65))
 
 mt[, alevel2 := (alevel / 2) %>% round(digits = 1)]
 
