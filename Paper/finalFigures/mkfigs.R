a52b768dfa2de48ded3e267938522a5c830de7dd
Lee Richman <leepr@upenn.edu>
2022-02-10 11:54:14 -0500

    rasterize figs

--- Paper/finalFigures/mkfigs.R
+++ Paper/finalFigures/mkfigs.R
@@ -1,4 +1,4 @@
 library(pdftools)
 
 # combine PDFs, the file names will already be in order
-pdf_combine(list.files("Paper/finalFigures/", pattern = "(F|S)[0-9]", full.names = TRUE), output = "Paper/finalFigures/Figures.pdf")
+pdf_combine(list.files("Paper/finalFigures/Rasterized", pattern = "(F|S)[0-9]", full.names = TRUE), output = "Paper/finalFigures/Figures_raster.pdf")
