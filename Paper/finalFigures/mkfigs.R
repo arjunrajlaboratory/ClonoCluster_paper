8019f756a99d36d1874ebee9019a44d71e4323ba
Lee Richman <leepr@upenn.edu>
2022-12-03 17:18:53 -0500

    add supplemental figures with legends per journal

--- Paper/finalFigures/mkfigs.R
+++ Paper/finalFigures/mkfigs.R
@@ -2,3 +2,6 @@ library(pdftools)
 
 # combine PDFs, the file names will already be in order
 pdf_combine(list.files("Paper/finalFigures/Rasterized", pattern = "(F|S)[0-9]", full.names = TRUE), output = "Paper/finalFigures/Figures_raster.pdf")
+
+
+#pdf_combine(list.files("Paper/finalFigures/Rasterized/With_legends", pattern = "S[0-9]", full.names = TRUE), output = "Paper/finalFigures/Figures_supplemental_w_legends.pdf")
